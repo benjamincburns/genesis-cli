5ddbb3760ed3a59cb7dbe0637bef21e72f31b2e0
n8wb <vanish172@gmail.com>
2019-12-24 08:18:07 -0800

    wip

--- cmd/genesis/main.go
+++ cmd/genesis/main.go
@@ -1,5 +1,28 @@
 package main
 
+import(
+	"log"
+
+	"github.com/whiteblock/genesis-cli/pkg/oauth2-noserver"
+
+	"golang.org/x/oauth2"
+)
 func main(){
-	
+		conf := &oauth2.Config{
+		ClientID:     "cli",            // also known as client key sometimes
+		//ClientSecret: "___________________________", // also known as secret key
+		Scopes:       []string{"offline_access"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://auth.infra.whiteblock.io/auth/realms/wb/protocol/openid-connect/auth",
+			TokenURL: "https://auth.infra.whiteblock.io/auth/realms/wb",
+		},
+	}
+
+	client, err := oauth2ns.AuthenticateUser(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// use client.Get / client.Post for further requests, the token will automatically be there
+	_, _ = client.Get("/auth-protected-path")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/whiteblock/genesis-cli
 
 go 1.13
+
+require (
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.11 // indirect
+	github.com/nmrshll/oauth2-noserver v0.0.0-20190221200101-9bf017bef639
+	github.com/nmrshll/rndm-go v0.0.0-20170430161430-8da3024e53de
+	github.com/palantir/stacktrace v0.0.0-20161112013806-78658fd2d177
+	github.com/skratchdot/open-golang v0.0.0-20190402232053-79abb63cd66e
+	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6
+)
