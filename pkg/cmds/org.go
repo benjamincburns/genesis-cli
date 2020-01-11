5aa872306de17b4d4cb68d7f0fd77514065bbf9a
n8wb <vanish172@gmail.com>
2020-01-10 18:10:31 -0800

    add org command

---
+++ pkg/cmds/org.go
@@ -0,0 +1,29 @@
+package cmds
+
+import (
+	"github.com/whiteblock/genesis-cli/pkg/organization"
+	"github.com/whiteblock/genesis-cli/pkg/util"
+
+	"github.com/spf13/cobra"
+)
+
+var orgCmd = &cobra.Command{
+	Use:   "org",
+	Short: "What org am I acting as?",
+	Long: `What org am I acting as?`,
+	Aliases: []string{},
+	Run: func(cmd *cobra.Command, args []string) {
+		util.CheckArguments(cmd, args, 0, 0)
+		org, err := organization.Get(orgNameOrId, client)
+		if err != nil {
+			util.ErrorFatal(err)
+		}
+
+		util.Print(org.Name)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(orgCmd)
+}
+
