405529e08a653d1063d63cd06a6ee7648aa7e16b
Antoine Toulme <antoine@lunar-ocean.com>
2020-01-13 11:15:45 -0800

    detect no tty for progress bar

--- pkg/cmds/internal/run.go
+++ pkg/cmds/internal/run.go
@@ -10,6 +10,23 @@ import (
 	"github.com/whiteblock/mpb"
 )
 
+func TrackRunStatusNoTTY(id string, total int64) {
+	for {
+		res, err := service.GetStatus(id)
+		if err != nil {
+			if strings.Contains(err.Error(), "could not find the status") {
+				continue
+			}
+			util.ErrorFatal(err)
+		}
+		util.Printf("%d%%", total - int64(res.StepsLeft))
+		if res.StepsLeft == 0 || res.Finished == true {
+			return
+		}
+		time.Sleep(5000 * time.Millisecond)
+	}
+}
+
 func TrackRunStatus(p *mpb.Progress, bar *mpb.Bar, id string, total int64) {
 	for {
 		res, err := service.GetStatus(id)
